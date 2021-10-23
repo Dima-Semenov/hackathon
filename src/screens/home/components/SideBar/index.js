7f9f0de5a53eb216e0c3810dd096bc381373337e
Dima Semenov <semenovdima998@gmail.com>
2021-10-23 16:29:00 +0300

    side bar is done

---
+++ src/screens/home/components/SideBar/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './styles.scss'
+
+export const SideBar = () => {
+	return (
+		<div className="sideBar">
+			<h2 className="sideBar__title">Films Blog</h2>
+
+			<p className="sideBar__search">Sort by</p>
+			<lu className="sideBar__list">
+				<li className="sideBar__item">
+					<span className="sideBar__icon sideBar__actors" />
+					Actors
+				</li>
+				<li className="sideBar__item">
+					<span className="sideBar__icon sideBar__films" />
+					Movies
+				</li>
+				<li className="sideBar__item">
+					<span className="sideBar__icon sideBar__series" />
+					Series
+				</li>
+				<li className="sideBar__item">
+					<span className="sideBar__icon sideBar__genres" />
+					Genres
+				</li>
+			</lu>
+		</div>
+	)
+}
