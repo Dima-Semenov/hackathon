945c6c55d7f85adc19430a76cd94d71abf40abbb
yamchinsky <66372922+yamchinsky@users.noreply.github.com>
2021-10-23 14:27:03 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import Header from './Components/header/Header'
 import { getAllBlogs, setAllBlogs } from './redux/slices/blogSlice'
 
 import Home from './screens/home/container/index'
@@ -19,6 +20,7 @@ function App() {
 		<div className="App">
 			{value}
 			<Home />
+			<Header />
 		</div>
 	)
 }
