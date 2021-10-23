fb219d0f81afdb7af3830e596924cf638772f04d
yamchinsky <66372922+yamchinsky@users.noreply.github.com>
2021-10-23 23:13:46 +0300

    authadded

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Redirect, Route, Switch } from 'react-router'
+import { ToastContainer, Zoom } from 'react-toastify'
+import { useDispatch } from 'react-redux'
+import 'react-toastify/dist/ReactToastify.css'
+
+import { getCurrentUser } from './redux/slices/blogSlice/auth/auth.operations'
 
 import { CurrentBlog, Home } from './screens'
 import './App.scss'
-import './assets/scss/style.scss';
+import './assets/scss/style.scss'
+import AuthPage from './screens/authForm/AuthForm'
 
 function App() {
+	const dispatch = useDispatch()
+	useEffect(() => {
+		dispatch(getCurrentUser())
+	}, [dispatch])
 	return (
 		<div className="App container">
 			<Switch>
+				<Route
+					path="/
+				"
+					component={AuthPage}
+				/>
 				<Route path="/home" component={Home} />
 				<Route path="/blog/:id" component={CurrentBlog} />
 
-				<Redirect to="/home" />
+				<Redirect
+					to="/
+				"
+				/>
 			</Switch>
+			<ToastContainer
+				transition={Zoom}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+			/>
 		</div>
 	)
 }
